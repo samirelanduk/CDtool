dedde8be001ee3376794ba6d533fb1612f45e6a9
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-20 12:55:06 +0000

    Restart FTs

---
+++ ftests/test_meta_site.py
@@ -0,0 +1,14 @@
+from .base import FunctionalTest
+from time import sleep
+
+class HomePageTests(FunctionalTest):
+
+    def test_home_page_general_structure(self):
+        pass
+
+
+
+class HelpTests(FunctionalTest):
+
+    def test_help_page_general_structure(self):
+        pass
---
+++ ftests/test_single_cd_processing.py
@@ -0,0 +1,35 @@
+from .base import FunctionalTest
+from time import sleep
+
+class AveragingSeriesTests(FunctionalTest):
+
+    def test_can_submit_single_blank_file(self):
+        pass
+
+
+    def test_can_submit_multi_scan_single_blank_file(self):
+        pass
+
+
+    def test_can_submit_multiple_blank_files(self):
+        pass
+
+
+    def test_graceful_errors_on_blank_submission_tests(self):
+        pass
+
+
+    def test_can_submit_single_sample_file(self):
+        pass
+
+
+    def test_can_submit_multi_scan_single_sample_file(self):
+        pass
+
+
+    def test_can_submit_multiple_sample_files(self):
+        pass
+
+
+    def test_graceful_errors_on_sample_submission_tests(self):
+        pass
