de0207dcd5a88247143b3cf9c2741221123d7c6b
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-21 17:24:32 +0000

    Spruce up output file

---
+++ cdprocessing/file_templates.py
@@ -0,0 +1,6 @@
+data_file = """CDtool %s
+Date generated: %s
+Time generated: %s
+
+Wavelength (nm)   Absorbance
+"""
--- cdprocessing/views.py
+++ cdprocessing/views.py
@@ -1,7 +1,9 @@
 import json
+from datetime import datetime
 from django.shortcuts import render
 from django.http.response import HttpResponse
-from cdprocessing import functions
+from cdprocessing import functions, file_templates
+from cdtool import version
 
 # Create your views here.
 def home_page(request):
@@ -13,7 +15,12 @@ def single_run(request):
         if "series" in request.POST:
             series = json.loads(request.POST["series"])
             lines = ["{}   {}".format(line[0], line[1]) for line in series]
-            response = HttpResponse("\n".join(lines), content_type='application/plain-text')
+            header = file_templates.data_file % (
+             version,
+             datetime.now().strftime("%d %B, %Y (%A)"),
+             datetime.now().strftime("%H:%M:%S (UK Time)")
+            )
+            response = HttpResponse(header + "\n".join(lines), content_type='application/plain-text')
             response['Content-Disposition'] = 'attachment; filename="average_blank.dat"'
             return response
         lines = functions.clean_file(list(request.FILES["blank"]))
