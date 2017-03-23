1e09cc54a9885546ad760ddd0e746dbc7e996fc6
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-23 14:56:35 +0000

    Add footer

--- ftests/test_meta_site.py
+++ ftests/test_meta_site.py
@@ -4,7 +4,17 @@ from time import sleep
 class HomePageTests(FunctionalTest):
 
     def test_home_page_general_structure(self):
-        pass
+        # The user goes to the home page
+        self.browser.get(self.live_server_url)
+
+        # There are four main sections to the body
+        body = self.browser.find_element_by_tag_name("body")
+        self.assertEqual(
+         [element.tag_name for element in body.find_elements_by_xpath("./*")],
+         ["header", "nav", "main", "footer"]
+        )
+
+        # The footer has three
 
 
 
