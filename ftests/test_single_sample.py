2c1f29897ea20c8278c184e1fd9f8d5035ecd213
Sam Ireland <sam.ireland.uk@gmail.com>
2017-06-29 15:11:15 +0100

    Add check for gen file processing

--- ftests/test_single_sample.py
+++ ftests/test_single_sample.py
@@ -8,6 +8,7 @@ class SingleScanTests(FunctionalTest):
     def test_can_crunch_single_scan(self):
         # Get expected data
         input_data = self.get_single_scan_from_file("single-sample.dat")
+
         # The user goes to the main page
         self.get("/")
 
@@ -111,7 +112,78 @@ class SingleScanTests(FunctionalTest):
 
 
     def test_can_crunch_single_gen_scan(self):
-        pass
+        # Get expected data
+        input_data = self.get_single_gen_scan_from_file("single-gen-sample.gen")
+
+        # The user goes to the main page
+        self.get("/")
+
+        # There is an input section but no output section
+        inputdiv = self.browser.find_element_by_id("input")
+        self.assertEqual(len(self.browser.find_elements_by_id("output")), 0)
+
+        # There is a single sample input div
+        sampleinputs = inputdiv.find_elements_by_class_name("sample-input")
+        self.assertEqual(len(sampleinputs), 1)
+        sampleinput = sampleinputs[0]
+
+        # The sample input has a div for uploading scans
+        scansinputs = sampleinput.find_elements_by_class_name("scans-input")
+        self.assertEqual(len(scansinputs), 1)
+        scansinput = scansinputs[0]
+
+        # The scans input has inputs for files and sample name
+        file_input, name_input = scansinput.find_elements_by_tag_name("input")[:2]
+
+        # A single scan is provided
+        file_input.send_keys(BASE_DIR + "/ftests/test_data/single-gen-sample.gen")
+        name_input.send_keys(".gen sample")
+
+        # There is a configuration div
+        configdiv = inputdiv.find_element_by_id("input-config")
+
+        # The user enters the experiment name
+        exp_name_input = configdiv.find_elements_by_tag_name("input")[0]
+        exp_name_input.send_keys("Test Experiment")
+
+        # The user submits the data
+        submit_button = inputdiv.find_elements_by_tag_name("input")[-1]
+        submit_button.click()
+
+        # The user is still on the same page
+        self.check_page("/")
+
+         # There is now an output section
+        output_div = self.browser.find_element_by_id("output")
+
+        # It has sections for the chart, for configuration, and downloading
+        chart_div = output_div.find_element_by_id("chart")
+        config_div = output_div.find_element_by_id("chart-config")
+        download_div = output_div.find_element_by_id("download")
+
+        # The chart section has a chart in it
+        self.check_chart_appears(chart_div)
+
+        # The chart has the correct title
+        self.check_chart_title(chart_div, "Test Experiment")
+
+        # The chart x axis goes from 190 to 280
+        self.check_chart_x_axis(190, 280)
+
+        # There is a single line series
+        self.check_visible_line_series_count(chart_div, 1)
+
+        # The line series matches the scan in the input data
+        self.check_line_matches_data("sample0", [w[:2] for w in input_data])
+
+        # There is a single error series
+        self.check_visible_area_series_count(chart_div, 1)
+
+        # The error series matches the scan error in the input data
+        self.check_error_matches_data(
+         "sample_error0",
+         [[w[0], w[1] - w[2], w[1] + w[2]] for w in input_data]
+        )
 
 
     def test_error_on_no_file_sent(self):
