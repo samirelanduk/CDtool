80d68979342713d30f1e00fe2e43f5fa5814aa5b
Sam Ireland <sam.ireland.uk@gmail.com>
2017-10-18 14:56:11 +0100

    Add check that old cdtool .gen files work with subtraction

--- ftests/test_single_sample.py
+++ ftests/test_single_sample.py
@@ -219,11 +219,37 @@ class SingleScanSingleBlankTests(FunctionalTest):
         self.check_file_download_ok("two_scan_subtraction_experiment.dat", input_data)
 
 
-    '''def test_can_crunch_scan_and_blank_gens(self):
-        pass
+    def test_can_crunch_scan_and_blank_gens(self):
+        # Get expected data
+        input_data = self.get_old_gen_data("single-old-gen.gen")
+        baseline_data = self.get_old_gen_data("single-old-gen-baseline.gen")
+        input_data = self.subtract(input_data, baseline_data)
+
+        # The user goes to the main page
+        self.get("/")
+
+        # The user inputs a single AVIV scan and a single AVIV baseline scan
+        self.input_data(
+         files="single-old-gen.gen",
+         baseline_files="single-old-gen-baseline.gen",
+         sample_name="A bi-scan sample",
+         exp_name="Two Gen Subtraction Experiment"
+        )
+
+        # The user is still on the same page
+        self.check_page("/")
+
+        # There is now an output section
+        self.check_output_section_there()
+
+        # The chart section has a chart in it
+        self.check_chart_ok("Two Gen Subtraction Experiment", 190, 280, input_data[::-1])
+
+        # The download section produces a file
+        self.check_file_download_ok("two_gen_subtraction_experiment.dat", input_data)
 
 
-    def test_error_on_no_blank_file_given(self):
+    '''def test_error_on_no_blank_file_given(self):
         pass
 
 
